78fc46e7d62c6d2117506ce6e32547f0ac9da7cc
‘liuliuyalong <liuyalong@wondertek.com.cn>
2017-10-16 16:56:15 +0800

    修改文件路径

--- core-ad/src/main/java/com/wondertek/mobilevideo/bc/core/service/AuthVerifyLogManager.java
+++
@@ -1,7 +0,0 @@
-package com.wondertek.mobilevideo.bc.core.service;
-
-import com.wondertek.mobilevideo.bc.core.model.AuthVerifyLog;
-
-public interface AuthVerifyLogManager extends GenericManager<AuthVerifyLog, Integer>{
-	 
-}
