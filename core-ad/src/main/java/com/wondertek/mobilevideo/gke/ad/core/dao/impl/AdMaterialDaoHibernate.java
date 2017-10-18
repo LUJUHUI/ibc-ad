50d1f2d8192fb6650c56c6f4c764a002f80c6978
liuyalong@wondertek.com.cn <liuyalong>
2017-10-18 10:32:27 +0800

    commit

--- core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/dao/impl/AdMaterialDaoHibernate.java
+++ core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/dao/impl/AdMaterialDaoHibernate.java
@@ -1,5 +1,6 @@
 package com.wondertek.mobilevideo.gke.ad.core.dao.impl;
 
+import com.wondertek.mobilevideo.gke.ad.core.dao.AdMaterialDao;
 import com.wondertek.mobilevideo.gke.ad.core.model.AdMaterial;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
  * create by lujuhui 2017/10/18 9:48
  */
 @Repository
-public class AdMaterialDaoHibernate extends GenericDaoHibernate<AdMaterial,Long>{
+public class AdMaterialDaoHibernate extends GenericDaoHibernate<AdMaterial,Long> implements AdMaterialDao{
     public AdMaterialDaoHibernate() {
         super(AdMaterial.class);
     }
