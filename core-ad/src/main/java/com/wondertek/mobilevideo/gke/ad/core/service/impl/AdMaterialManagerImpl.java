404a67eb41e0070823c792cef4e3ecdce73c73d3
lujuhui <lujuhui@wondertek.com.cn>
2017-10-18 10:21:02 +0800

    commit

--- core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/service/impl/AdMaterialManagerImpl.java
+++ core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/service/impl/AdMaterialManagerImpl.java
@@ -3,11 +3,15 @@ package com.wondertek.mobilevideo.gke.ad.core.service.impl;
 import com.wondertek.mobilevideo.gke.ad.core.dao.AdMaterialDao;
 import com.wondertek.mobilevideo.gke.ad.core.model.AdMaterial;
 import com.wondertek.mobilevideo.gke.ad.core.service.AdMaterialManger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdMaterialManagerImpl extends GenericManagerImpl<AdMaterial,Long> implements AdMaterialManger {
 
+    @Autowired
     private AdMaterialDao adMaterialDao;
-
+    @Autowired
     public AdMaterialManagerImpl(AdMaterialDao adMaterialDao){
         super(adMaterialDao);
         this.adMaterialDao = adMaterialDao;
