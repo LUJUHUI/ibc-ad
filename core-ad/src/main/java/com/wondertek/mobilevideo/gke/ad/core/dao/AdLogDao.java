039389c3513ebcd35bc89a2785c16474074bfbdf
lvliuzhong <lvliuzhong@wangxing>
2017-10-19 19:15:26 +0800

    commit

---
+++ core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/dao/AdLogDao.java
@@ -0,0 +1,14 @@
+package com.wondertek.mobilevideo.gke.ad.core.dao;
+
+
+import java.util.List;
+import java.util.Map;
+
+import com.wondertek.mobilevideo.gke.ad.core.model.AdAd;
+import com.wondertek.mobilevideo.gke.ad.core.model.AdLog;
+import com.wondertek.mobilevideo.gke.ad.core.model.AdSlot;
+import com.wondertek.mobilevideo.gke.ad.core.utils.PageList;
+
+public interface AdLogDao extends GenericDao<AdLog, Long>{
+	
+}
---
+++ core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/dao/impl/AdLogDaoHibernate.java
@@ -0,0 +1,20 @@
+package com.wondertek.mobilevideo.gke.ad.core.dao.impl;
+
+import com.wondertek.mobilevideo.gke.ad.core.dao.AdAdDao;
+import com.wondertek.mobilevideo.gke.ad.core.dao.AdLogDao;
+import com.wondertek.mobilevideo.gke.ad.core.dao.AdSlotDao;
+import com.wondertek.mobilevideo.gke.ad.core.model.AdAd;
+import com.wondertek.mobilevideo.gke.ad.core.model.AdLog;
+import com.wondertek.mobilevideo.gke.ad.core.model.AdSlot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdLogDaoHibernate extends GenericDaoHibernate<AdLog, Long> implements AdLogDao{
+
+	public AdLogDaoHibernate() {
+		super(AdLog.class);
+	}
+	
+}
