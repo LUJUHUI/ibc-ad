661624febfc64926cfb0ab36ff56c1e5c7152af0
lvliuzhong <lvliuzhong@wangxing>
2017-12-04 10:48:47 +0800

    commit

---
+++ core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/utils/impl/RedisServiceImpl.java
@@ -0,0 +1,12 @@
+package com.wondertek.mobilevideo.gke.ad.core.utils.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.wondertek.mobilevideo.gke.ad.core.utils.RedisService;
+
+/**
+ * Created by liuya on 2017/11/16.
+ */
+public class RedisServiceImpl extends RedisService<String,String>{
+    
+}
