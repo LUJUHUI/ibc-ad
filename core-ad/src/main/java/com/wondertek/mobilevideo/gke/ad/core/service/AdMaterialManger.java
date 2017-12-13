e45a1570ee812b707cfec51ceff1ee5716afa61a
lvliuzhong <lvliuzhong@wangxing>
2017-12-13 10:13:45 +0800

    uploadPic

--- core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/service/AdMaterialManger.java
+++ core-ad/src/main/java/com/wondertek/mobilevideo/gke/ad/core/service/AdMaterialManger.java
@@ -1,8 +1,15 @@
 package com.wondertek.mobilevideo.gke.ad.core.service;
 
+import java.io.File;
+import java.util.List;
+
 import com.wondertek.mobilevideo.gke.ad.core.model.AdMaterial;
 
 public interface AdMaterialManger extends GenericManager<AdMaterial,Integer> {
 
     public void verify(String ids, int type,String userName)throws RuntimeException;
+
+	public void saveAdMaterial(AdMaterial adMaterial, List<File> uploadPic, List<String> uploadPicFileName,String username);
+
+	public void updateAdMaterial(AdMaterial adMaterial,String username);
 }
